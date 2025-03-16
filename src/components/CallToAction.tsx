ea5160e0869942f983d3010d32304a0a50562243
rudra1614 <rudra.r.naik@outlook.com>
2025-03-16 17:28:46 +0530

    Update CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 
@@ -11,12 +11,16 @@ const CallToAction = () => {
           Join our community of passionate social workers and NGOs committed to creating positive change around the world.
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4">
-          <Button size="lg" variant="secondary" className="px-8" onClick="/login">
+          <Link to="/login">
+             <Button size="lg" variant="secondary" className="px-8" >
             Find Opportunities
-          </Button>
-          <Button size="lg" className="bg-white text-ngo-blue hover:bg-gray-100 px-8" onClick="/organization/login">
+             </Button>
+          </Link>
+          <Link to="/organization/login">
+             <Button size="lg" className="bg-white text-ngo-blue hover:bg-gray-100 px-8" onClick="/organization/login">
             Post a Project
-          </Button>
+             </Button>
+          </Link>
         </div>
       </div>
     </section>
