d614fb9fa738b1e526c55044e3941bb25076313e
rudra1614 <rudra.r.naik@outlook.com>
2025-03-16 16:39:01 +0530

    Update(2)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
         <div className="w-full md:w-1/2 mt-10 md:mt-0 relative z-10">
           <div className="relative">
             <img 
-              src="public\lovable-uploads\NGO 1 (2).jpg" 
+              src="/lovable-uploads/NGO 1 (2).jpg" 
               alt="Social worker helping in a community garden" 
               className="rounded-lg shadow-xl max-w-full md:ml-auto"
             />
