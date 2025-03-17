e300c06b92ab33cbd42842a796a1f53b3eaaf4dc
rudra1614 <rudra.r.naik@outlook.com>
2025-03-17 20:58:15 +0530

    About Page(2)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
+import FeaturedOpportunities from '@/components/FeaturedOpportunities';
+import HowItWorks from '@/components/HowItWorks';
+import WhyChooseUs from '@/components/WhyChooseUs';
+import StatsAndPortal from '@/components/StatsAndPortal';
+import Testimonials from '@/components/Testimonials';
+import CallToAction from '@/components/CallToAction';
+import Footer from '@/components/Footer';
 
 const AboutPage: React.FC = () => {
   return (
