e0cfdb036bfabc257912a76274b1e36bd4e38590
rudra1614 <rudra.r.naik@outlook.com>
2025-03-17 21:08:47 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import FeaturedOpportunities from '@/components/FeaturedOpportunities';
